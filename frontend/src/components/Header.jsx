3c9ae101759e68a01be9196c968e245adc54c9b4
sammwaura <meshsami27@gmail.com>
2023-11-01 16:58:26 +0300

    add search bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import { FaSearch } from 'react-icons/fa';
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>AirBnB-</span>
             <span className='text-slate-700'>BookingApp</span>
         </h1>
-        <form>
-            <input type="text" placeholder='Search.....' />
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type="text" placeholder='Search.....' className='bg-transparent' />
+                <FaSearch className='text-slate-600'/>
         </form>
        </div> 
     </header>
